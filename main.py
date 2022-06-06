bb263e6c61c85b3c45d31abd8536067f7058fe85
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2022-06-06 23:24:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import 
 
 df = pd.read_csv('fake_job_postings.csv')
 print(df.head())
 
+print(df.info())
+print(df.fraudulent.value_counts())
+print(df.nunique())
+df.plot(x="industry", y="fraudulent", kind="bar", color="purple")
+plt.show()
