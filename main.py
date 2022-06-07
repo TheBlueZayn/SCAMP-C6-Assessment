bd96ed04713c266a55062a74508156db3a70f0bf
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2022-06-07 18:12:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import 
 
 df = pd.read_csv('fake_job_postings.csv')
-print(df.head())
 
-print(df.info())
-print(df.fraudulent.value_counts())
-print(df.nunique())
-df.plot(x="industry", y="fraudulent", kind="bar", color="purple")
+plt.plot()
+
+# print(df.head())
+
+# print(df.info())
+# print(df.fraudulent.value_counts())
+# print(df.nunique())
+
+x = df.groupby("industry")["fraudulent"].sum()
+print(x)
+x.plot( kind="bar", color="purple")
+
+# plt.xticks(rot=45)
+# plt.show()
+
+plt.bar(x, [1,1000])
 plt.show()
