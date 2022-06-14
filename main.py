181e7c8fd8143e957aef163b1b86df2140529a93
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2022-06-14 10:03:49 +0100

    Changed to jupyter notebook
    Use of markdowns to aid understanding the analysis

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Import libraries
+from bs4 import Stylesheet
+from matplotlib import style
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
 
 # Load CSv into a DataFrame
 df = pd.read_csv('fake_job_postings.csv')
@@ -19,9 +22,13 @@ x = df.groupby("industry")["fraudulent"].sum().sort_values(ascending=False)[:15]
 print(x)
 
 # Plot, label accordingly and show the series
-x.plot(kind="barh",color="purple")
+#fig, ax = plt.subplots(figsize=(15, 6))
+plt.style.use("seaborn-pastel")
+x.plot(kind="barh",
+       color="purple",
+       fontsize=8)
 plt.xlabel("Number of fraudulent job postings")
-plt.title("Number of Fraudulent job postings per industry")
+plt.title("Fraudulent Job Postings Per Industry")
 plt.show()
 
 # Look up the unique values of "salary-range"
@@ -34,13 +41,16 @@ df["salary_range"].replace(["9-De", "3-Apr", "4-Apr", "Oct-15", "4-Jun", "10-Oct
 print(df.info())
 
 
-# Looking into the null values in each column and plotting 
-df.isna().sum().sort_values()[:10].plot(kind="bar", rot=45)
+# Looking into the null values in each column and plotting it out
+df.isna().sum().sort_values()[:10].plot(kind="bar", 
+                                        rot=45, 
+                                        figsize=(15,6), 
+                                        logx=True)
 plt.xlabel("Count")
 plt.title("Distribution of null values")
 plt.show()
 
-df["department"].unique()
+
 
 
 print(df['department'].where(df['fraudulent'] == 1))
